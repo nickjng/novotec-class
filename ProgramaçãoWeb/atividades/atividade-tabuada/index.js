133977fce7e2c3ffc391e42902072014d5ece2ae
nickjng <nickolas.ng@outlook.com>
2023-08-04 00:35:36 -0300

    primeiros passos no gimp

--- ProgramaçãoWeb/atividades/atividade-tabuada/index.js
+++ ProgramaçãoWeb/atividades/atividade-tabuada/index.js
@@ -24,5 +24,45 @@ function calcularTabuada() {
             `
         }
 
+
+
+
+
+
+
+
+
+
+
+        let a = 2;
+        let b = 5;
+        let c = 3;
+
+        let delta = (b * b) - 4 * a * c;
+
+        if (delta < 0) {
+            console.log("A equação não possui raízes reais.");
+        } else if (delta === 0) {
+            let x = -b / (2 * a);
+            console.log("A equação possui uma raiz real: x = " + x);
+        } else {
+            let x1 = (-b + Math.sqrt(delta)) / (2 * a);
+            let x2 = (-b - Math.sqrt(delta)) / (2 * a);
+            console.log("A equação possui duas raízes reais: x1 = " + x1 + ", x2 = " + x2);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
