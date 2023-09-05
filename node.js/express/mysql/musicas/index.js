e40b7ea5dd3d5332cf5c01c87edc15da500978c3
nickjng <nickolas.goncalves@bandtec.com.br>
2023-09-05 12:35:18 -0300

    insert

--- node.js/express/mysql/musicas/index.js
+++ node.js/express/mysql/musicas/index.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const exphbs = require('express-handlebars')
+const mysql  = require('mysql')
 const app = express();
 
+const conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "bancodedados",
+    database: "aulanode"
+})
+
+conn.connect((err)=>{
+    if(err){
+        console.log("deu ruim");
+        console.log(err);
+    }
+    else{
+        console.log("Conectado ao Banco de dados");
+    }
+})
 const hbs = exphbs.create({
     partialsDir: ['views/partials'] // --> Uso do Partials 
 })
@@ -17,7 +34,15 @@ router.get("/cadastrar", (req, res) =>{
 
 
 router.post("/salvar", (req, res) =>{
+    const nome = req.body.nome;
+    const artista = req.body.artista
+    const tipo = req.body.tipo;
 
+    const sql = `insert into musica (nome, artista, tipo)
+                values ('${nome}','${artista}','${tipo}');`
+
+
+    conn.query(sql)
     console.log(req.body)
 
 })
